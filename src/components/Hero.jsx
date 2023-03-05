edd3f6cbade5586fdf8d1e21d50f08484ef8a236
Swayze <theartintheraw@gmail.com>
2023-03-05 12:33:26 -0500

    typos and alerations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,21 +28,22 @@ const Hero = () => {
       <LaptopCanvas className='overflow-y-hidden' />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
+        {/* <a href='#about'> */}
+        <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <motion.div
+            animate={{
+              y: [0, 24, 0],
+            }}
+            transition={{
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: "loop",
+            }}
+            className='w-3 h-3 rounded-full bg-secondary mb-1'
+          />
+          <p className={`${styles.sectionSubText}`}>scroll</p>
+        </div>
+        {/* </a> */}
       </div>
     </section>
   );
